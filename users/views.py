c9c6c20d54d9c133fed86e1f880e283a3597bfe6
tonibosch <toni.bosch@estudiantat.upc.edu>
2023-04-26 23:09:55 +0200

    Afegir boto guardar foto

--- users/views.py
+++ users/views.py
@@ -75,3 +75,20 @@ def edit_user_profile_view(request):
 
     context = {'profile': profile}
     return render(request, 'user_configuration.html', context)
+
+@login_required
+def change_picture_profile_view(request):
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        profile_picture = request.FILES.get('image')
+        if profile_picture:
+            # Guardar la imagen en el modelo Profile
+            profile.image = profile_picture
+            profile.save()
+            print(profile.image.url)
+
+    context = {
+        'profile': profile
+    }
+    return render(request, 'user_configuration.html', context)
