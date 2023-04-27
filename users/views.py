20fd29fe10b29694f3b755090f6dd88f6b81afd8
javierRodriguezUPC <120645986+javierRodriguezUPC@users.noreply.github.com>
2023-04-27 23:12:08 +0200

    issues solved

--- users/views.py
+++ users/views.py
@@ -59,7 +59,7 @@ def edit_user_profile_view(request):
     profile = None
     try:
         profile, created = Profile.objects.get_or_create(
-            user=request.user, url="https://issuetracker2asw.s3.eu-west-3.amazonaws.com/media/mistery-man-gravatar-wordpress-avatar-persona-misteriosa-510x510.png"
+            user=request.user
         )
     except:
         print("Error al crear el perfil")
@@ -84,7 +84,8 @@ def change_picture_profile_view(request):
             picture = Picture()
             picture.File = profile_picture
             picture.save()
-            profile.url = picture.File.url
+            profile.url = picture.File.url.split('?')[0]
+            profile.save()
 
     context = {
         'profile': profile, 'image_url': profile.url.split('?')[0]
