9da4903978212282e92e7e8bc19fa80d7ed15661
paucarbopascual <pau.carbo.pascual@estudiantat.upc.edu>
2023-05-18 21:01:48 +0200

    upd

--- issuetracker2/settings.py
+++ issuetracker2/settings.py
@@ -94,15 +94,24 @@ WSGI_APPLICATION = 'issuetracker2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'd8v4meuh81pmib',
+       'USER': 'qpapnvnnlqbsby',
+       'PASSWORD': '85dfd952523714b0ad376d414c683877cda862b240005cede70804170dade6a9',
+       'HOST': 'ec2-63-34-69-123.eu-west-1.compute.amazonaws.com',
+       'PORT': '5432',
+   }
 }
 
-db_from_env = dj_database_url.config(default='postgres://qpapnvnnlqbsby:85dfd952523714b0ad376d414c683877cda862b240005cede70804170dade6a9@ec2-63-34-69-123.eu-west-1.compute.amazonaws.com:5432/d8v4meuh81pmib')
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
