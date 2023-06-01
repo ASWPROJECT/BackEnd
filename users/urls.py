b9c61585baf3d242b249ab3a4fa0ab3cb895d2a2
santilara <santilaracelis16@gmail.com>
2023-06-01 18:52:40 +0200

    NEW ENDPOINT USER PROFILE

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token),  
     path('change-picture-profile/', api.ChangePictureProfileView.as_view()),  
     path('edit-user-profile/', api.UserProfileView.as_view()),  
+    path('id/', api.ViewUserProfile.as_view()),  
+
 
 ]
